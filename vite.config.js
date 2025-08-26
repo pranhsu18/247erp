29b654036c937ff1c16bf7db96f26f651ef26afc
pranhsu18 <pranshu82870@gmail.com>
2025-08-26 06:48:11 +0530

    hh

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // base: './', // 👈 Add this if deploying to subpath or seeing blank screen
+});
